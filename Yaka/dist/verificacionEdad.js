5ac976152ab82e23aef21f90641497a50c04fc6d
Elid Reyes <ereyes@uniempresarial.edu.co>
2025-03-26 10:42:32 -0500

    Fusionando cambios de la rama Alejandro en Nath

--- Yaka/dist/verificacionEdad.js
+++ Yaka/dist/verificacionEdad.js
@@ -1,14 +1,16 @@
 function confirmAge(isOfAge) {
+    console.log(isOfAge); // Verifica si está recibiendo true o false
     if (isOfAge) {
-        // localStorage.setItem("ageVerified", "true");
+        localStorage.setItem("ageVerified", "true");
         document.getElementById("age-verification").style.display = "none";
     } else {
         window.location.href = "https://www.google.com"; // Redirige a otro sitio si es menor
     }
 }
 
+/* 
 window.onload = function() {
     if (localStorage.getItem("ageVerified") === "true") {
         document.getElementById("age-verification").style.display = "none";
     }
-} 
+} */
